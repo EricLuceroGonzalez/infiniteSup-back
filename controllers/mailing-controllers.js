4535b62bfd0288cd4438765c3e9a7b08cd471edd
Eric Lucero Gonzalez <ericlucerogonzalez@gmail.com>
2020-08-15 18:00:31 -0500

    mailing controller with new html template

--- controllers/mailing-controllers.js
+++ controllers/mailing-controllers.js
@@ -36,35 +36,95 @@ const sendMail = async (req, res, next) => {
         from: process.env.mailUser, // sender address
         to: [`ericlucerogonzalez@gmail.com`], // list of receivers
         subject: `infinite-supplies.com `, // Subject line
-        html: `<h3
-        style="
-          text-shadow: 3px 2px 1px black;
-          color: white;
-          background-color: #0411a3;
-          font-weight: bold;
-          padding: 18px 8px;
-          font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS',
-            sans-serif;
-          box-shadow: 6px 6px 5px #4c91de;
-        "
-      >
-        INFINITE-SUPPLIES.COM
-        <span role="img" aria-label="rocket">
-          📨
-        </span>
-      </h3>
-      <div
-      style="
-        background-color: #2cf0bf65;
-        border: 3px solid #0411a3;
-        font-weight: 500;
-        font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
-        padding: 4rem 4rem;
-      "
-    >
-        <div>Nombre: ${name} </div>
-        <div>Mensaje: ${message} </div>
-        <div>Enviado: ${date} </div>
+        html: `<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@900&display=swap" rel="stylesheet"> 
+        <div
+        style="width: 80%;
+        border: 3px solid #0411a3;    
+        margin: 2px auto">
+        <div
+          style="
+            text-shadow: 3px 2px 1px black;
+            color: white;
+        margin: 3rem auto;
+        text-align: center;
+          "
+        >
+        <img
+        style="max-width: 180px;"
+        src="https://res.cloudinary.com/dcvnw6hvt/image/upload/v1596351802/InfiniteSupplies/identity/navlogo_dpncuq.png"
+        alt='infinite logo'
+        ></img>
+        </div>
+        
+        <div
+          style="
+          margin-top: 3rem;
+            background-color: #9e9efd;
+            font-weight: 500;
+            font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+            padding: 4rem 4rem;
+          "
+        >
+          <div
+          style="font-family: 'Montserrat', sans-serif;">
+          <h2>Hola, ${name}</h2>
+          Hemos recibido su mensaje:</div>
+          <div
+            style="
+              width: 90%;
+              margin: 1rem auto 6rem auto;
+              padding: 2rem 1rem;
+              background-color: #9e9efd;
+              font-family: Montserrat, sans-serif;"
+          >
+            ${message}
+          </div>
+          <div
+          style="font-family: 'Montserrat', sans-serif;">
+          Con mucho gusto le estaremos respondiendo en breve.</div>
+        </div>
+        <div
+        style="width: 100%;
+        text-align: center;
+        margin: 3rem auto">
+          <img
+          style="max-width: 70px;"
+          src='https://res.cloudinary.com/dcvnw6hvt/image/upload/v1595808047/InfiniteSupplies/identity/logo_sm_cut_nxb9bs.png'
+          alt='logo infinite single ball'
+          ></img>
+          <div style="
+          width: 70%;
+          margin: 2rem auto;
+          display: grid;  grid-template-columns: auto auto auto;
+          ">
+            <div class="grid-item">    <img src="https://img.icons8.com/fluent/48/000000/instagram-new.png"/></div>
+            <div class="grid-item"><img src="https://img.icons8.com/nolan/64/email.png"/></div>
+            <div class="grid-item"><img src="https://img.icons8.com/nolan/64/phone.png"/></div>
+            <div class="grid-item">
+              <a 
+              style="text-decoration: none; 
+              font-family: 'Montserrat', sans-serif;
+              font-size: 0.7rem;
+              color: #9e9efd"
+              href="https://www.instagram.com/infinitesuppliessa/">
+                @infinitesuppliessa</a>
+            </div>
+            <div class="grid-item">
+              <a
+              style="text-decoration: none; 
+              font-size: 0.7rem;
+              font-family: 'Montserrat', sans-serif;
+              color: #9e9efd"
+               href="mailto: info@infinite-supplies.com">info@infinite-supplies.com</a>
+            </div>
+            <div 
+            style="text-decoration: none; 
+            font-family: 'Montserrat', sans-serif;
+            font-size: 0.7rem;
+            color: #9e9efd"
+            class="grid-item">(+507) 6291-7954</div>
+          </div>   
+        </div>
         </div>`,
       },
       async (error, info) => {
@@ -77,48 +137,96 @@ const sendMail = async (req, res, next) => {
               from: process.env.mailUser, // sender address
               to: [email], // list of receivers
               subject: `infinite-supplies.com | Hola ${name}`, // Subject line
-              html: `<h3
-              style="
-                text-shadow: 3px 2px 1px black;
-                color: white;
-                background-color: #0411a3;
-                font-weight: bold;
-                padding: 18px 8px;
-                font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS',
-                  sans-serif;
-                box-shadow: 6px 6px 5px #4c91de;
-                text-decoration: none;
-              "
-            >
-              INFINITE-SUPPLIES.COM
-              <span role="img" aria-label="rocket">
-                📨
-              </span>
-            </h3>
-            <div
-              style="
-                background-color: #2cf0bf65;
-                border: 3px solid #0411a3;
-                font-weight: 500;
-                font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
-                padding: 4rem 4rem;
-              "
-            >
-              <div><span style="font-weight: bolder;">Hola, ${name}</span></div>
-              <div>Hemos recibido su mensaje:</div>
+              html: `<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@900&display=swap" rel="stylesheet"> 
+              <div
+              style="width: 80%;
+              border: 3px solid #0411a3;    
+              margin: 2px auto">
               <div
                 style="
-                  width: 50%;
-                  margin: 1rem auto 5rem auto;
-                  padding: 2rem 1rem;
-                  background-color: #9e9efd;
-                  border: 3px solid #0411a3;
+                  text-shadow: 3px 2px 1px black;
+                  color: white;
+              margin: 3rem auto;
+              text-align: center;
                 "
               >
-                ${message}
+              <img
+              style="max-width: 180px;"
+              src="https://res.cloudinary.com/dcvnw6hvt/image/upload/v1596351802/InfiniteSupplies/identity/navlogo_dpncuq.png"
+              alt='infinite logo'
+              ></img>
               </div>
-              <div>Con mucho gusto le estaremos respondiendo en breve.</div>
-            </div>`,
+              
+              <div
+                style="
+                margin-top: 3rem;
+                  background-color: #9e9efd;
+                  font-weight: 500;
+                  font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+                  padding: 4rem 4rem;
+                "
+              >
+                <div
+                style="font-family: 'Montserrat', sans-serif;">
+                <h2>Hola, ${name}</h2>
+                Hemos recibido su mensaje:</div>
+                <div
+                  style="
+                    width: 90%;
+                    margin: 1rem auto 6rem auto;
+                    padding: 2rem 1rem;
+                    background-color: #9e9efd;
+                    font-family: Montserrat, sans-serif;"
+                >
+                  ${message}
+                </div>
+                <div
+                style="font-family: 'Montserrat', sans-serif;">
+                Con mucho gusto le estaremos respondiendo en breve.</div>
+              </div>
+              <div
+              style="width: 100%;
+              text-align: center;
+              margin: 3rem auto">
+                <img
+                style="max-width: 70px;"
+                src='https://res.cloudinary.com/dcvnw6hvt/image/upload/v1595808047/InfiniteSupplies/identity/logo_sm_cut_nxb9bs.png'
+                alt='logo infinite single ball'
+                ></img>
+                <div style="
+                width: 70%;
+                margin: 2rem auto;
+                display: grid;  grid-template-columns: auto auto auto;
+                ">
+                  <div class="grid-item">    <img src="https://img.icons8.com/fluent/48/000000/instagram-new.png"/></div>
+                  <div class="grid-item"><img src="https://img.icons8.com/nolan/64/email.png"/></div>
+                  <div class="grid-item"><img src="https://img.icons8.com/nolan/64/phone.png"/></div>
+                  <div class="grid-item">
+                    <a 
+                    style="text-decoration: none; 
+                    font-family: 'Montserrat', sans-serif;
+                    font-size: 0.7rem;
+                    color: #9e9efd"
+                    href="https://www.instagram.com/infinitesuppliessa/">
+                      @infinitesuppliessa</a>
+                  </div>
+                  <div class="grid-item">
+                    <a
+                    style="text-decoration: none; 
+                    font-size: 0.7rem;
+                    font-family: 'Montserrat', sans-serif;
+                    color: #9e9efd"
+                     href="mailto: info@infinite-supplies.com">info@infinite-supplies.com</a>
+                  </div>
+                  <div 
+                  style="text-decoration: none; 
+                  font-family: 'Montserrat', sans-serif;
+                  font-size: 0.7rem;
+                  color: #9e9efd"
+                  class="grid-item">(+507) 6291-7954</div>
+                </div>   
+              </div>
+              </div>`,
             },
             (error, info) => {
               //              console.log(`error: ${error}`);
